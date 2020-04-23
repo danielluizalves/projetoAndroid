361c8befd5fd7c36ba68f8d84ac37619a39c60e7
Daniel L. Alves <daniel@clinicarx.com.br>
2020-04-22 23:00:39 -0300

    Insere menu no projeto

--- app/src/main/java/com/example/rentalcar/aluguelActivity.java
+++ app/src/main/java/com/example/rentalcar/aluguelActivity.java
@@ -1,13 +1,20 @@
 package com.example.rentalcar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+
 
 public class aluguelActivity extends AppCompatActivity {
 
@@ -28,6 +35,34 @@ public class aluguelActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.carro:
+                Toast.makeText(getApplicationContext(), "Você ja está na página do carro...", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.registro:
+                Intent intent2 = new Intent(getApplicationContext(), RegistroActivity.class);
+                startActivity(intent2);
+                return true;
+            case R.id.toaster:
+                Toast.makeText(getApplicationContext(), "Exibindo Taoster", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     //INICIALIZAÇÃO DOS COMPONENTES
